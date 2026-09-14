34773fc0b6577e41e293761abb7128dc280446e6
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2026-09-14 09:11:33 +0600

    Implement twoSum function in solution.py

---
+++ 1.Two sum/solution.py
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (seen.find(complement) != seen.end()) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
